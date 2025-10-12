12b06147ce62b9af0c019280b8e92a6ccf9bb8e1
SMAH1 <integral22003@yahoo.com>
2025-10-12 16:53:27 +0330

    feat: Add db-query page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dbquery',
+    loadComponent: () => import('./db-query/db-query.component').then(m => m.DbQueryComponent)
+  }
+];
---
+++ src/app/db-query/db-query.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-db-query',
+  standalone: true,
+  imports: [],
+  templateUrl: './db-query.component.html',
+  styleUrl: './db-query.component.css'
+})
+export class DbQueryComponent {
+
+}
